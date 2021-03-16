1808fddc0d6d6e4eb152fcc245186c9b195d6d60
vijaykumar-vk10 <49861687+vijaykumar-vk10@users.noreply.github.com>
2021-03-16 16:36:09 +0530

    Add files via upload

--- ImdbChartFetcher.js
+++ ImdbChartFetcher.js
@@ -2,67 +2,75 @@ const fetch = require('node-fetch')
 const cheerio = require('cheerio');
 
 const ImdbChartFetcher = async () => {
-    
-    /** Get values from command line */
-    const args = process.argv.slice(2) ;
-    const chartUrl = args[0];
-    const itemsCount = parseInt(args[1]);
+    try{
 
-    /** Request html from url*/
-    let imdbResponse  = await fetch(chartUrl);
-    let htmlData = await imdbResponse.text();
-    let $ = cheerio.load(htmlData);
+        /** Get values from command line */
+        const args = process.argv.slice(2) ;
+        const chartUrl = args[0];
+        const itemsCount = parseInt(args[1]);
 
-    let movies = [];
-    const resultJson = [];
-    /** Loop through each element in table and get title */ 
-    $("#main > div > span > div > div > div.lister > table > tbody > tr > td.titleColumn > a").each((index, element) => {
-        if(index >= itemsCount){
-            return false;
-        }
-        movies.push($(element).attr('href'));
-    });
+        /** Request html from url*/
+        let imdbResponse  = await fetch(chartUrl);
+        let htmlData = await imdbResponse.text();
+        let $ = cheerio.load(htmlData);
 
-    /** Loop through the array of movies based on the count */
-    for(let i=0 ;i<itemsCount ; i++){   
-        imdbResponse  = await fetch(`https://www.imdb.com${movies[i]}`);
-        htmlData = await imdbResponse.text();
-        $ = cheerio.load(htmlData);
-
-        /** Get values of required fields */
-        let $title = $('.title_wrapper h1');
-        let title = $title.first().contents().filter(function() {
-            return this.type === 'text';
-        }).text().trim();
-
-        let movie_release_year = $("#titleYear > a").text();
-        let imdb_rating =  $('span[itemProp="ratingValue"]').text();
-        let summary = $('div.summary_text').text().trim();
-        let duration = $("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time").text().trim();
-
-        let genre = [];
-        $("#titleStoryLine > div ").each((index, element) => { 
-            if($(element).text().trim().includes("Genres:")){
-                genre = $(element).text().trim().split(":")
-                genre = genre[1].split("|")
+        let movies = [];
+        const resultJson = [];
+        /** Loop through each element in table and get title */ 
+        $("#main > div > span > div > div > div.lister > table > tbody > tr > td.titleColumn > a").each((index, element) => {
+            if(index >= itemsCount){
                 return false;
-            } 
-        })
-        for(j=0 ; j<genre.length ; j++){
-            genre[j] = genre[j].replace("\n","").trim();
+            }
+            movies.push($(element).attr('href'));
+        });
+
+        /** Loop through the array of movies based on the count */
+        for(let i=0 ;i<itemsCount ; i++){   
+            imdbResponse  = await fetch(`https://www.imdb.com${movies[i]}`);
+            htmlData = await imdbResponse.text();
+            $ = cheerio.load(htmlData);
+
+            /** Get values of required fields */
+            let $title = $('.title_wrapper h1');
+            let title = $title.first().contents().filter(function() {
+                return this.type === 'text';
+            }).text().trim();
+
+            let movie_release_year = $("#titleYear > a").text();
+            let imdb_rating =  $('span[itemProp="ratingValue"]').text();
+            let summary = $('div.summary_text').text().trim();
+            let duration = $("#title-overview-widget > div.vital > div.title_block > div > div.titleBar > div.title_wrapper > div > time").text().trim();
+
+            let genre = [];
+            $("#titleStoryLine > div ").each((index, element) => { 
+                if($(element).text().trim().includes("Genres:")){
+                    genre = $(element).text().trim().split(":")
+                    genre = genre[1].split("|")
+                    return false;
+                } 
+            })
+            for(j=0 ; j<genre.length ; j++){
+                genre[j] = genre[j].replace("\n","").trim();
+            }
+
+            /** Push rating json to result object */
+            resultJson.push({
+                title,
+                movie_release_year,
+                imdb_rating,
+                summary,
+                duration,
+                genre
+            });
         }
 
-        /** Construct rating json */
-        resultJson.push({
-            title,
-            movie_release_year,
-            imdb_rating,
-            summary,
-            duration,
-            genre
-        });
+        /** Print the response */
+        console.log(resultJson)
+
     }
-    console.log(resultJson)
+    catch(error){
+        console.log("error occured. Please try again")
+    }
 } 
 
 ImdbChartFetcher()
